fb63cfb02f37e667f49958c762e789dcec8e9ba9
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-16 11:30:37 +0100

    copy squash-00-live.sh into chroot

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -172,7 +172,7 @@ createInitrd() {
 	# fugly hack to get /dev/disk/by-label working
 	$SUDO sed -i -e "s@KERNEL!="sr*", IMPORT{builtin}="blkid"@KERNEL@#KERNEL" "$1"/lib/udev/rules.d/60-persistent-storage.rules
 	
-	$SUDO install -c -m 755 $OURDIR/create-liveramfs.sh $OURDIR/dracut-00-live.sh "$1"/boot/
+	$SUDO install -c -m 755 $OURDIR/create-liveramfs.sh $OURDIR/squash-00-live.sh "$1"/boot/
 	$SUDO chroot "$1" /boot/create-liveramfs.sh "$LABEL" "$KERNEL_ISO"
 	$SUDO rm "$1"/boot/create-liveramfs.sh
 	$SUDO rm "$1"/boot/dracut-00-live.sh
