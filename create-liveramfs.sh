6faf96c83d0d5a4f9d4b21cd1617007cb22e3b89
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-16 12:08:32 +0100

    create-liveramfs.sh do not inject *-00-live.sh for a test

--- create-liveramfs.sh
+++ create-liveramfs.sh
@@ -10,15 +10,15 @@ dracut -f --no-early-microcode --nofscks --noprelink --mount "tmpfs /var/tmp tmp
 
 # Add a mount script for the Live system -- we want an AUFS union
 # of squashfs and tmpfs...
-cd /boot
-rm -rf tmp
-mkdir tmp
-cd tmp
-xzcat ../liveinitrd.img |cpio -idu
-mkdir -p lib/dracut/hooks/mount
-sed -e "s,@LABEL@,$LABEL,g" ../squash-00-live.sh >lib/dracut/hooks/mount/00-live.sh
-chmod 0755 lib/dracut/hooks/mount/00-live.sh
-find . |cpio -R 0:0 -H newc -o --quiet |xz --check=crc32 --lzma2=dict=1MiB >../liveinitrd.img 
-rm -rf tmp
-cd -
+#cd /boot
+#rm -rf tmp
+#mkdir tmp
+#cd tmp
+#xzcat ../liveinitrd.img |cpio -idu
+#mkdir -p lib/dracut/hooks/mount
+#sed -e "s,@LABEL@,$LABEL,g" ../squash-00-live.sh >lib/dracut/hooks/mount/00-live.sh
+#chmod 0755 lib/dracut/hooks/mount/00-live.sh
+#find . |cpio -R 0:0 -H newc -o --quiet |xz --check=crc32 --lzma2=dict=1MiB >../liveinitrd.img 
+#rm -rf tmp
+#cd -
 ls -l /boot/liveinitrd.img
