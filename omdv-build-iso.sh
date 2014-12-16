5a04bb00908cefb3e3d14a37dfd311a1bf9d1fd6
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-16 15:33:43 +0100

    fix typo

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -175,7 +175,7 @@ createInitrd() {
 	$SUDO install -c -m 755 $OURDIR/create-liveramfs.sh $OURDIR/squash-00-live.sh "$1"/boot/
 	$SUDO chroot "$1" /boot/create-liveramfs.sh "$LABEL" "$KERNEL_ISO"
 	$SUDO rm "$1"/boot/create-liveramfs.sh
-	$SUDO rm "$1"/boot/squash-00-live.s
+	$SUDO rm "$1"/boot/squash-00-live.sh
 	# get it back to original
 	# fugly hack to get /dev/disk/by-label working
 	$SUDO sed -i -e "s@#KERNEL!="sr*", IMPORT{builtin}="blkid"@#KERNEL@KERNEL" "$1"/lib/udev/rules.d/60-persistent-storage.rules
