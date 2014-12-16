acd77837d65b247b7570522af02d5c3574697451
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-16 16:42:10 +0100

    squash-00-live.sh fix mount options

--- squash-00-live.sh
+++ squash-00-live.sh
@@ -9,9 +9,9 @@ modprobe squashfs
 # try to mount the iso inide liveramfs
 # /run is mounted as tmpfs already
 mkdir -m 0755 -p /run/initramfs/live /run/live-ro /run/live-rw
-mount -n -t squashfs -o ro /dev/disk/by-label/@LABEL@  /run/initramfs/live
+mount -n -t auto -o ro /dev/disk/by-label/@LABEL@  /run/initramfs/live
 # mount squashfs image
-mount -o loop /run/initramfs/live/LiveOS/squashfs.img /run/live-ro
+mount -n -t squashfs /run/initramfs/live/LiveOS/squashfs.img /run/live-ro
 # mount tmpfs space as rw
 mount -n -t tmpfs tmpfs /run/live-rw
 # mount aufs as new root
