6836789119c8a1e3aea28068ed64435cdb0edb5a
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-16 15:20:30 +0100

    rewrite squash-00-live.sh

--- squash-00-live.sh
+++ squash-00-live.sh
@@ -6,14 +6,13 @@ modprobe iso9660
 modprobe loop
 modprobe squashfs
 
-if [ ! -d /run/initramfs/live/LiveOS/squashfs.img ]; then
-	mkdir -m 0755 -p /run/iso
-	mount /dev/disk/by-label/@LABEL@ /run/iso
-	mount -o loop /run/iso/LiveOS/squashfs.img /run/live-ro
-else
-	mount -o loop /run/initramfs/live/LiveOS/squashfs.img /run/live-ro
-fi
-
-mkdir -m 0755 -p /run/live-ro /run/live-rw
+# try to mount the iso inide liveramfs
+# /run is mounted as tmpfs already
+mkdir -m 0755 -p /run/initramfs/live /run/live-ro /run/live-rw
+mount -n -t squashfs -o ro /dev/disk/by-label/@LABEL@  /run/initramfs/live
+# mount squashfs image
+mount -o loop /run/initramfs/live/LiveOS/squashfs.img /run/live-ro
+# mount tmpfs space as rw
 mount -n -t tmpfs tmpfs /run/live-rw
+# mount aufs as new root
 mount -t aufs -o br=/run/live-ro:/run/live-rw "$NEWROOT"
