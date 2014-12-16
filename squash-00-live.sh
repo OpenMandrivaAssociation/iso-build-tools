83aee7d84141b105cb693e1475375fb905ec8c98
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-16 22:16:22 +0100

    check for /dev/disk/by-label before tryting to do anything

--- squash-00-live.sh
+++ squash-00-live.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+/#!/bin/sh
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 type det_fs >/dev/null 2>&1 || . /lib/fs-lib.sh
 
@@ -6,13 +6,17 @@ modprobe iso9660
 modprobe loop
 modprobe squashfs
 
-# try to mount the iso inide liveramfs
-# /run is mounted as tmpfs already
-mkdir -m 0755 -p /run/initramfs/live /run/live-ro /run/live-rw
-mount -n -t auto -o ro /dev/disk/by-label/@LABEL@  /run/initramfs/live
-# mount squashfs image
-mount -n -t squashfs /run/initramfs/live/LiveOS/squashfs.img /run/live-ro
-# mount tmpfs space as rw
-mount -n -t tmpfs tmpfs /run/live-rw
-# mount aufs as new root
-mount -t aufs -o br=/run/live-ro:/run/live-rw "$NEWROOT"
+if [ -e /dev/disk/by-label/@LABEL@ ]; then
+    # try to mount the iso inide liveramfs
+    # /run is mounted as tmpfs already
+    mkdir -m 0755 -p /run/initramfs/live /run/live-ro /run/live-rw
+    mount -n -t auto -o ro /dev/disk/by-label/@LABEL@  /run/initramfs/live
+    # mount squashfs image
+    mount -n -t squashfs /run/initramfs/live/LiveOS/squashfs.img /run/live-ro
+    # mount tmpfs space as rw
+    mount -n -t tmpfs tmpfs /run/live-rw
+    # mount aufs as new root
+    mount -t aufs -o br=/run/live-ro:/run/live-rw "$NEWROOT"
+else
+    echo "/dev/disk/by-label/@LABEL@ does not exists. Exiting."
+fi
