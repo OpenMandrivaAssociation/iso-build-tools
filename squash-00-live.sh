f905a5e823bfb60fa0ef3b15381f6850f7062dba
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-16 12:17:42 +0100

    seems like iso is mounted in /run/initramfs

--- squash-00-live.sh
+++ squash-00-live.sh
@@ -6,10 +6,14 @@ modprobe iso9660
 modprobe loop
 modprobe squashfs
 
-mkdir -m 0755 -p /run/iso /run/live-ro /run/live-rw
-mount /dev/disk/by-label/@LABEL@ /run/iso
-mount -o loop /run/iso/LiveOS/squashfs.img /run/live-ro
+if [ ! -d /run/initramfs/live/LiveOS/squashfs.img ]; then
+	mkdir -m 0755 -p /run/iso
+	mount /dev/disk/by-label/@LABEL@ /run/iso
+	mount -o loop /run/iso/LiveOS/squashfs.img /run/live-ro
+else
+	mount -o loop /run/initramfs/live/LiveOS/squashfs.img /run/live-ro
+fi
+
+mkdir -m 0755 -p /run/live-ro /run/live-rw
 mount -n -t tmpfs tmpfs /run/live-rw
-
 mount -t aufs -o br=/run/live-ro:/run/live-rw "$NEWROOT"
-
