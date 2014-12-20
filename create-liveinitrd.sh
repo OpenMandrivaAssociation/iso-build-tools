66e479a94ed884bf88e13b4ba1ccfca2e4718388
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-21 00:49:46 +0100

    fix path to dracut config for liveinitrd, more fixes for liveinitrd

--- create-liveinitrd.sh
+++ create-liveinitrd.sh
@@ -6,7 +6,7 @@ KERNEL_ISO="$2"
 [ -z "$LABEL" ] && LABEL="OpenMandriva"
 
 # build liveramfs and by default mount aufs on /sysroot
-dracut -f --no-early-microcode --nofscks --noprelink  /boot/liveinitrd.img --confdir /etc/draklive-install.d/isobuild/ $KERNEL_ISO
+dracut -f --no-early-microcode --nofscks --noprelink  /boot/liveinitrd.img --conf /etc/dracut.conf.d/60-dracut-isobuild.conf $KERNEL_ISO
 
 # Add a mount script for the Live system -- we want an AUFS union
 # of squashfs and tmpfs...
@@ -29,7 +29,7 @@ sleep 1
 # fugly hack to get /dev/disk/by-label
 sed -i -e '/KERNEL!="sr\*\", IMPORT{builtin}="blkid"/s/KERNEL/#KERNEL/g' -e '/TEST=="whole_disk", GOTO="persistent_storage_end"/s/TEST/#TEST/g' lib/udev/rules.d/60-persistent-storage.rules
 chmod 0755 lib/dracut/hooks/mount/00-live.sh
-find . |cpio -R 0:0 -H newc -o --quiet |xz --check=crc32 --lzma2=dict=1MiB >../liveinitrd.img 
+find . |cpio -R 0:0 -H newc -o --quiet |xz --check=crc32 --lzma2=dict=1MiB >../liveinitrd.img
 rm -rf tmp
 cd -
 ls -l /boot/liveinitrd.img
