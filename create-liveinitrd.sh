7c07831ef888e9559948cc04126a0172de046b09
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-21 19:19:03 +0100

    run dracut with LC_ALL=C for liveinitrd, fix logic for initrd hacks

--- create-liveinitrd.sh
+++ create-liveinitrd.sh
@@ -6,7 +6,7 @@ KERNEL_ISO="$2"
 [ -z "$LABEL" ] && LABEL="OpenMandriva"
 
 # build liveramfs and by default mount aufs on /sysroot
-dracut -f --no-early-microcode --nofscks --noprelink  /boot/liveinitrd.img --conf /etc/dracut.conf.d/60-dracut-isobuild.conf $KERNEL_ISO
+LC_ALL=C dracut -f --no-early-microcode --nofscks --noprelink  /boot/liveinitrd.img --conf /etc/dracut.conf.d/60-dracut-isobuild.conf $KERNEL_ISO
 
 # Add a mount script for the Live system -- we want an AUFS union
 # of squashfs and tmpfs...
@@ -20,9 +20,9 @@ sleep 1
 
 sed -e "s,@LABEL@,$LABEL,g" ../squash-00-live.sh >lib/dracut/hooks/mount/00-live.sh
 # be sure lib/dracut/hooks/mount/00-live.sh is there
-if [ -f lib/dracut/hooks/mount/00-live.sh ]; then
+if [ ! -f lib/dracut/hooks/mount/00-live.sh ]; then
     cp -f ../squash-00-live.sh lib/dracut/hooks/mount/00-live.sh
-    sed -e "s,@LABEL@,$LABEL,g" lib/dracut/hooks/mount/00-live.sh
+    sed -i -e "s,@LABEL@,$LABEL,g" lib/dracut/hooks/mount/00-live.sh
 fi
 
 sleep 1
