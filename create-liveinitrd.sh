914d62318f44ad9d151c027ced8135d680bce4ef
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-12-22 14:56:06 +0100

    try to fix by-label

--- create-liveinitrd.sh
+++ create-liveinitrd.sh
@@ -27,7 +27,7 @@ fi
 
 sleep 1
 # fugly hack to get /dev/disk/by-label
-sed -i -e '/KERNEL!="sr\*\", IMPORT{builtin}="blkid"/s/KERNEL/#KERNEL/g' -e '/TEST=="whole_disk", GOTO="persistent_storage_end"/s/TEST/#TEST/g' lib/udev/rules.d/60-persistent-storage.rules
+sed -i -e '/KERNEL!="sr\*\", IMPORT{builtin}="blkid"/s/KERNEL\!/KERNEL/g' -e '/TEST=="whole_disk", GOTO="persistent_storage_end"/s/TEST/#TEST/g' lib/udev/rules.d/60-persistent-storage.rules
 chmod 0755 lib/dracut/hooks/mount/00-live.sh
 find . |cpio -R 0:0 -H newc -o --quiet |xz --check=crc32 --lzma2=dict=1MiB >../liveinitrd.img
 rm -rf tmp
